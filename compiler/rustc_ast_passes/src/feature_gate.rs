f0529978c6613fe6f81b5ebc52bcdeedfca517a0
varkor <github@varkor.com>
2020-04-15 12:07:22 +0100

    Make pre-expansion gate slightly less obnoxious

--- compiler/rustc_ast_passes/src/feature_gate.rs
+++ compiler/rustc_ast_passes/src/feature_gate.rs
@@ -629,7 +629,11 @@ pub fn check_crate(krate: &ast::Crate, sess: &Session) {
     gate_all!(const_trait_bound_opt_out, "`?const` on trait bounds is experimental");
     gate_all!(const_trait_impl, "const trait impls are experimental");
     gate_all!(half_open_range_patterns, "half-open range patterns are unstable");
-    gate_all!(destructuring_assignment, "destructuring assignments are unstable");
+    if parse_sess.span_diagnostic.err_count() == 0 {
+        // Errors for `destructuring_assignment` can get quite noisy, especially where `_` is
+        // involved, so we only emit errors where there are no other parsing errors.
+        gate_all!(destructuring_assignment, "destructuring assignments are unstable");
+    }
 
     // All uses of `gate_all!` below this point were added in #65742,
     // and subsequently disabled (with the non-early gating readded).
